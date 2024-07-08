2d05485ec1010c271353972656e07ae67a1a1145
Chethana Potukanam <142076224+ChethanaPotukanam@users.noreply.github.com>
2024-07-08 22:31:48 +0530

    Time: 36 ms (60.19%), Space: 16.7 MB (8.01%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,18 +1,33 @@
 import math
 MAX_INT = 2**31 - 1
+MIN_INT = -2**31
 class Solution:
     def reverse(self, x: int) -> int:
-        temp = abs(x)
-        rev = 0
-        while(temp > 0):
-            r = temp % 10
-            rev = rev * 10 + r
-            if(rev>MAX_INT):
+        MAX_INT = 2 ** 31 - 1 # 2,147,483,647
+        MIN_INT = -2 ** 31    #-2,147,483,648
+        reverse = 0
+
+        while x != 0:
+            if reverse > MAX_INT / 10 or reverse < MIN_INT / 10:
                 return 0
-            temp=temp//10
-        if(x<0):
-            return -rev
-        return rev
+            digit = x % 10 if x > 0 else x % -10
+            reverse = reverse * 10 + digit
+            # x = x // 10
+            # don't use above line for negative numbers use math.trun function
+            x=math.trunc(x/10)
+        return reverse
+        
+        # temp = abs(x)
+        # rev = 0
+        # while(temp > 0):
+        #     r = temp % 10
+        #     rev = rev * 10 + r
+        #     if(rev>MAX_INT):
+        #         return 0
+        #     temp=temp//10
+        # if(x<0):
+        #     return -rev
+        # return rev
 
         # If there is no limit on size
         # temp = abs(x)
